c7124ce25406ae4111fe22ff7d48c6fa1a0842be
kaas991 <j.neele@heliomare-onderwijs.nl>
2025-11-20 10:45:24 +0000

    evrting shut be rady

--- src/fbx_test.cpp
+++ src/fbx_test.cpp
@@ -1,7 +1,20 @@
-#include "fbxsdk.h"
+#include <fbxsdk.h>
 
-int main() {
 
+/**
+ * Main function - loads the hard-coded fbx file,
+ * and prints its contents in an xml format to stdout.
+ */
+int main(int argc, char** argv) {
+
+    // Change the following filename to a suitable filename value.
+    const char* lFilename = "file.fbx";
     
+    // Initialize the SDK manager. This object handles all our memory management.
+    FbxManager* lSdkManager = FbxManager::Create();
+  
+    
+    // Destroy the SDK manager and all the other objects it was handling.
+    lSdkManager->Destroy();
     return 0;
 }
