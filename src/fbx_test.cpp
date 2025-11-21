94fea825c1b145cedc5bbe0256254c2b49505dba
Jasper <bink.neele@gmail.com>
2025-11-21 20:51:06 +0100

    windows suport

--- src/fbx_test.cpp
+++ src/fbx_test.cpp
@@ -43,18 +43,56 @@ int main(int argc, char** argv) {
     lRootNode->AddChild(lTestNode);
 
     //divine mesh
-    FbxVector4 vertex0(500, 0, 500);
-    FbxVector4 vertex1(-500, 0, 500);
-    FbxVector4 vertex2(500, 0, -500);
-    FbxVector4 vertex3(-500, 0, -500);
+    // Define the eight corners of the cube.
+    // The cube spans from
+    //    -50 to  50 along the X axis
+    //      0 to 100 along the Y axis
+    //    -50 to  50 along the Z axis
+    FbxVector4 vertex0(-50, 0, 50);
+    FbxVector4 vertex1(50, 0, 50);
+    FbxVector4 vertex2(50, 100, 50);
+    FbxVector4 vertex3(-50, 100, 50);
+    FbxVector4 vertex4(-50, 0, -50);
+    FbxVector4 vertex5(50, 0, -50);
+    FbxVector4 vertex6(50, 100, -50);
+    FbxVector4 vertex7(-50, 100, -50);
 
-    lTestMesh->InitControlPoints(4);
+    // Initialize the control point array of the mesh.
+    lTestMesh->InitControlPoints(24);
+    FbxVector4* lControlPoints = lTestMesh->GetControlPoints();
 
-    lTestMesh->SetControlPointAt(vertex0, 0);
-    lTestMesh->SetControlPointAt(vertex1, 1);
-    lTestMesh->SetControlPointAt(vertex2, 2);
-    lTestMesh->SetControlPointAt(vertex3, 3);
-    
+    // Define each face of the cube.
+    // Face 1
+    lControlPoints[0] = vertex0;
+    lControlPoints[1] = vertex1;
+    lControlPoints[2] = vertex2;
+    lControlPoints[3] = vertex3;
+    // Face 2
+    lControlPoints[4] = vertex1;
+    lControlPoints[5] = vertex5;
+    lControlPoints[6] = vertex6;
+    lControlPoints[7] = vertex2;
+    // Face 3
+    lControlPoints[8] = vertex5;
+    lControlPoints[9] = vertex4;
+    lControlPoints[10] = vertex7;
+    lControlPoints[11] = vertex6;
+    // Face 4
+    lControlPoints[12] = vertex4;
+    lControlPoints[13] = vertex0;
+    lControlPoints[14] = vertex3;
+    lControlPoints[15] = vertex7;
+    // Face 5
+    lControlPoints[16] = vertex3;
+    lControlPoints[17] = vertex2;
+    lControlPoints[18] = vertex6;
+    lControlPoints[19] = vertex7;
+    // Face 6
+    lControlPoints[20] = vertex1;
+    lControlPoints[21] = vertex0;
+    lControlPoints[22] = vertex4;
+    lControlPoints[23] = vertex5;
+
 
     // Export the scene to the file.
     lExporter->Export(lScene);
