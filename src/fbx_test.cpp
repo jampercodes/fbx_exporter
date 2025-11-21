0f22a435b578f4182d620e10c0a8eeecb5e05d9b
kaas991 <j.neele@heliomare-onderwijs.nl>
2025-11-21 10:22:14 +0000

    exporting a empty file

--- src/fbx_test.cpp
+++ src/fbx_test.cpp
@@ -8,12 +8,34 @@
 int main(int argc, char** argv) {
 
     // Change the following filename to a suitable filename value.
-    const char* lFilename = "file.fbx";
+    const char* lFilename = "test.fbx";
     
     // Initialize the SDK manager. This object handles all our memory management.
     FbxManager* lSdkManager = FbxManager::Create();
   
+    FbxIOSettings* ios = FbxIOSettings::Create(lSdkManager, IOSROOT);
+    lSdkManager->SetIOSettings(ios);
+
+
+    FbxExporter* lExporter = FbxExporter::Create(lSdkManager, "");
+
+
+    bool lExportStatus = lExporter->Initialize(lFilename, -1, lSdkManager->GetIOSettings());
     
+    if(!lExportStatus) {
+        printf("Call to FbxExporter::Initialize() failed.\n");
+        printf("Error returned: %s\n\n", lExporter->GetStatus().GetErrorString());
+        return 1;
+    }
+
+    // Create a new scene so it can be populated by the imported file.
+    FbxScene* lScene = FbxScene::Create(lSdkManager,"myScene");
+
+    // ... Import a scene, or build a new one ...
+
+    // Export the scene to the file.
+    lExporter->Export(lScene);
+
     // Destroy the SDK manager and all the other objects it was handling.
     lSdkManager->Destroy();
     return 0;
